3fe2ae340ba5cd178af7876b8eee7bf48b085529
万云乌云 <5297216+a1364241399a@user.noreply.gitee.com>
2023-06-03 16:16:39 +0800

    test0.0.1

---
+++ src/components/TestDemo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>33</div>
+</template>
+
+<script>
+export default {
+name:'testDemo',
+data(){
+    return {
+        msg:'Hello World'
+    }
+},
+create(){
+    console.log(this.msg)
+}
+}
+
+</script>
+<style  lang="scss" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,20 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 // 创建路由对象，参数是要使用的路由配置文件列表，每个文件都必须
-const routes = [
-  { path: "/", component: () => import("@/components/HelloWorld.vue") },
+const routes = [{ 
+    path: "/",
+    name: "home",
+    component: () => import("@/components/HelloWorld.vue") },
+  {
+    path:"/test",
+    name:"Test",
+    component: () => import("@/components/TestDemo.vue"), 
+  }
 ];
-export default createRouter({ 
-    history: createWebHashHistory(), 
-    routes 
+
+const router = createRouter({ 
+  history: createWebHistory(), 
+  routes 
 });
+
+export default router;
