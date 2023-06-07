93e0720254dadb2fd62e2388c2ba57b1361dc69e
万云乌云 <5297216+a1364241399a@user.noreply.gitee.com>
2023-06-07 16:31:44 +0800

    23060701

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,18 +1,17 @@
 <template>
-    <div>
-        <div class="common-layout">
+    <div class="common-layout">
+        <el-container class="box">
+            <el-aside :ref="aside" :width="asideWidth" :class="{wait: isAside,narrow: !isAside}">Aside</el-aside>
             <el-container>
-                
-                <el-aside width="auto">Aside</el-aside>
-                <el-container>
-                    <el-header>Header</el-header>
-                    <el-main>
-                        Main0
-                        <router-view></router-view>
-                    </el-main>
-                </el-container>
+                <el-header>Header
+                    <el-button type="primary" @click="clickTest">测试用</el-button>
+                </el-header>
+                <el-main>
+                    Main0
+                    <router-view></router-view>
+                </el-main>
             </el-container>
-        </div>
+        </el-container>
     </div>
 </template>
 
@@ -20,21 +19,73 @@
 export default {
     name: 'DefaultLayout',
     data() {
-        return {}
+        return {
+            isAside: false,
+            asideWidth: 200 + 'px'
+        }
     },
-    created(){
+    created() {
         console.info("sdf")
+    },
+    methods:{
+        clickTest(){
+            console.log(this.isAside)
+            this.isAside = !this.isAside;
+            console.info(this.$refs)
+            if(this.isAside){
+                this.asideWidth = 100+'px';
+            }else{
+                this.asideWidth = 200+'px';
+            }
+        }
     }
 }
 
 </script>
 <style  lang="scss" scoped>
 .common-layout {
- width: 100%;
- height: 100%;
-}
-.el-container {
     width: 100%;
     height: 100%;
 }
-</style>
+
+.box {
+
+    height: 100%;
+}
+
+.el-aside {
+    height: 100%;
+    background-color: antiquewhite;
+}
+.wait{
+    animation-name: wait;
+    animation-duration: 0.5s;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+
+}
+
+.narrow{
+    animation-name: narrow;
+    animation-duration: 0.5s;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+}
+
+@keyframes wait{
+    from {width : 200px}
+    to {width: 50px;}
+}
+@keyframes narrow{
+    from {width: 50px;}
+    to {width: 200px;}
+}
+.el-header {
+    height: 50px;
+    background-color: aliceblue;
+}
+
+.el-main {
+    height: calc(100% - 50px);
+    background-color: aqua;
+}</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // 路由器 创建一个路由器对象，用于处理请求和匹配路
-import './permission' // 权限验证器 如果你的项目中有这么一个库，请在你的库中注释掉上
+import './permission' // 权限验证器 使用该功能在页面加载时就绪后自动调用一次。不需要手动调用此函数
+import '@/assets/style/index.scss' // 全局样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css' 
 import {createStore}from "vuex"
